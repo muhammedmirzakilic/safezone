341395dee2a6ea0c7f2b18b23b3c70ece7b8a32b
Muhammed Mirza Kılıç <muhammedmirzakilic@Muhammeds-MacBook-Pro.local>
2018-09-22 20:01:21 +0300

    refactor

--- index.js
+++ index.js
@@ -3,6 +3,4 @@
 import { AppRegistry } from "react-native";
 import App from "./src/AppStart";
 import { name as appName } from "./app.json";
-//import Be from "./App";
-import AppAndroid from "./androidKontaktIO";
 AppRegistry.registerComponent(appName, () => App);
--- src/Scenes/Home/index.js
+++ src/Scenes/Home/index.js
@@ -1,96 +1,89 @@
 import React, { Component } from "react";
-import { View, Text, TouchableHighlight,StyleSheet,TextInput,Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  TextInput,
+  Image
+} from "react-native";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Button } from "react-native-elements";
 import Images from "../../Assets/Images";
-
+import Form from "./form";
 class Container extends Component {
   constructor(props) {
     super(props);
-    this.state= {};
+    this.state = {};
   }
   navigateToChooseAvatar = () => {
     this.props.dispatch(navigate("ChooseAvatar"));
   };
+  updateForm = (key, value) => {
+    this.setState({ [key]: value });
+  };
   render() {
     return (
-      <View style={{flex: 1,backgroundColor: "#221b4b"}}>
+      <View style={{ flex: 1, backgroundColor: "#221b4b" }}>
+        <View style={{ flex: 0.8, marginTop: 50 }}>
+          <Form {...this.state} updateForm={this.updateForm} />
+        </View>
 
-      <View style={{flex:0.8,marginTop:50}}>
-      <Text style={styles.TextStyleCss}>PNR</Text>
-      <TextInput style={styles.TextInputStyleClass}
-          placeholder="PNR Giriniz"
-          placeholderTextColor = "gray"
-          underlineColorAndroid='transparent'
-          onChangeText={(text) => this.setState({pnrText:text})}
-          value={this.state.pnrText}/>
-      <Text style={styles.TextStyleCss}>SOYADIN</Text>
-      <TextInput
-          placeholder="Soyadınızı Giriniz"
-          placeholderTextColor = "gray"
-          underlineColorAndroid='transparent'
-          style={styles.TextInputStyleClass}
-          onChangeText={(text) => this.setState({surnameText:text})}
-          value={this.state.surnameText}/>
-      <Image source={Images["giff"]} style={styles.gif} />
-      </View>
-
-      <View style={{flex:0.2,justifyContent: 'flex-end'}}>
-      <TouchableHighlight
-      style={styles.submit}
-      onPress={this.navigateToChooseAvatar}
-      underlayColor='#fff'>
-      <Text style={[styles.submitText]}>Oyuna Başla</Text>
-      </TouchableHighlight>
-      </View>
-
-
-
+        <View style={{ flex: 0.2, justifyContent: "flex-end" }}>
+          <TouchableHighlight
+            style={styles.submit}
+            onPress={this.navigateToChooseAvatar}
+            underlayColor="#fff"
+          >
+            <Text style={[styles.submitText]}>Oyuna Başla</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  TextStyleCss: {
+    textAlign: "center",
+    color: "#F5FCFF",
+    fontSize: 35,
+    fontWeight: "bold"
+  },
 
-  TextStyleCss:{
-    textAlign: 'center',color:"#F5FCFF",fontSize: 35,fontWeight: 'bold'
+  TextInputStyleClass: {
+    textAlign: "center",
+    height: 50,
+    borderWidth: 2,
+    borderColor: "#3fc9b0",
+    borderRadius: 20,
+    backgroundColor: "#221b4b",
+    marginRight: 5,
+    marginLeft: 5,
+    color: "#F5FCFF"
   },
-  
-  TextInputStyleClass:{
-  textAlign: 'center',
-  height: 50,
-   borderWidth: 2,
-   borderColor: '#3fc9b0',
-   borderRadius: 20 ,
-   backgroundColor : "#221b4b",
-   marginRight:5,
-   marginLeft:5,
-   color:'#F5FCFF'
-  },
-  
-  submit:{
-    marginRight:5,
-    marginLeft:5,
-    backgroundColor:'#3fc9b0',
-    borderRadius:10,
+
+  submit: {
+    marginRight: 5,
+    marginLeft: 5,
+    backgroundColor: "#3fc9b0",
+    borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: "#fff",
     height: 50
   },
-  submitText:{
-      color:'#fff',
-      textAlign:'center',
-      fontSize: 25,
-      fontWeight: 'bold'
+  submitText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold"
   },
   gif: {
     width: "100%",
     height: 200
-  },
-   
-  });
+  }
+});
 
 const mapStateToProps = state => {
   return {};
